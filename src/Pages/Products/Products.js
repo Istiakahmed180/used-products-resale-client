d74aabad8b64fd58bddc7a8582f251f4bb2892a6
Istiakahmed180 <istiakahmed180@gmail.com>
2022-11-25 10:34:19 +0600

    category products loaded on card details page

--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import ProductsCard from "./ProductsCard";
 
 const Products = () => {
   const products = useLoaderData();
-  console.log(products);
   return (
-    <div>
-      <h1>This Is Products</h1>
+    <div className="">
+      {products &&
+        products.map((product) => (
+          <ProductsCard key={product._id} product={product}></ProductsCard>
+        ))}
     </div>
   );
 };
---
+++ src/Pages/Products/ProductsCard.js
@@ -0,0 +1,68 @@
+import React from "react";
+
+const ProductsCard = ({ product }) => {
+  const {
+    picture,
+    name,
+    sellet_picture,
+    seller_name,
+    post_data,
+    location,
+    original_price,
+    resale_price,
+    use,
+  } = product;
+  return (
+    <div>
+      <div className="max-w-2xl my-14 mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
+        <img className="object-cover w-full h-64" src={picture} alt="Article" />
+
+        <div className="p-6">
+          <div>
+            <span className="text-xs font-medium text-blue-600 uppercase dark:text-blue-400">
+              {name}
+            </span>
+          </div>
+
+          <div className="mt-4">
+            <div className="flex items-center">
+              <div className="flex items-center">
+                <img
+                  className="object-cover h-10 rounded-full"
+                  src={sellet_picture}
+                />
+                <a
+                  href="#"
+                  className="mx-2 font-semibold text-gray-700 dark:text-gray-200"
+                  tabIndex="0"
+                  role="link"
+                >
+                  {seller_name}
+                </a>
+              </div>
+              <span className="mx-1 text-xs text-gray-600 dark:text-gray-300">
+                {post_data} <br />
+                Location: {location}
+              </span>
+            </div>
+            <div className="my-2 flex justify-between text-gray-600 dark:text-gray-300">
+              <small>
+                Original Price : $<del>{original_price}</del>
+              </small>{" "}
+              <br />
+              <span className="font-semibold">
+                Resale Price :{" "}
+                <span className="text-orange-400">${resale_price}</span>
+              </span>
+            </div>
+            <div className="my-2 text-center text-gray-600 dark:text-gray-300">
+              Product Using: {use}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductsCard;
